f716c61a5c9112490cfc5b8cb5d9a6e1289609c4
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-03-21 18:20:54 +0000

    added favicon and did js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,21 @@
 // Created by: Gabriel
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/** */
+
 function enterClicked() {
   // input
-  const streetName = document.getElementById("street-name").value
-  const userStreetNumber = parseInt(document.getElementById("street-number").value)
+  const lengthOfTriangle = document.getElementById("length-of-triangle").value
+  const widthOfTriangle = document.getElementById("width-of-triangle").value
+
+  //process
+  const answerStepOne = (lengthOfTriangle * widthOfTriangle)
+  const answerStepTwo = (answerStepOne / 2)
 
   // output
   document.getElementById("answer").innerHTML =
-    "Your info is: " + userStreetNumber + " " + streetName + "."
+    "Area of this triangle is: " + answerStepTwo + 
 }
