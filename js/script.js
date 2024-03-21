3415ae27366003c08e853e9d98bcdea270210f2c
Gabriel Zanardi Nogueira <159185296+Gabriel-Zanardi@users.noreply.github.com>
2024-03-21 18:01:37 +0000

    savepoint

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Gabriel
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+function enterClicked() {
+  // input
+  const streetName = document.getElementById("street-name").value
+  const userStreetNumber = parseInt(document.getElementById("street-number").value)
+
+  // output
+  document.getElementById("answer").innerHTML =
+    "Your info is: " + userStreetNumber + " " + streetName + "."
+}
